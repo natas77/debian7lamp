89bdf3ae35459209bd922cf8dc4131d565e5b049
natas77 <huzurp@yahoo.de>
2014-09-22 00:04:36 +0200

    Update debian764.sh

--- debian764.sh
+++ debian764.sh
@@ -7,6 +7,9 @@ apt-get -y install php5 mysql-server mysql-client phpmyadmin php5-curl curl git
 sudo service apache2 restart
 vagrant plugin install vagrant-vbox-snapshot
 composer create-project laravel/laravel laravel --prefer-dist
-composer create-project laravel/laravel laravel --prefer-dist
-composer create-project laravel/laravel laravel --prefer-dist
+
+chmod -R 777 app/storage
+
+chown -R :www-data app/storage
+
 
